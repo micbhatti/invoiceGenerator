83c55d822c5e734e5a21595aff2d8079012667bf
michael <michael@michaelmike.co>
2018-07-04 00:21:13 -0400

    add client table seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(InvoiceMakersTableSeeder::class);
+        $this->call(clientsTableSeeder::class);
     }
 }
